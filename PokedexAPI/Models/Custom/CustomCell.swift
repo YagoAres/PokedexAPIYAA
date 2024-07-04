93728f7fa75ba1e9002a5ad5aab3030a9c8744b2
YagoAres <yago.ares.armesto@accenture.com>
2024-07-04 15:54:11 +0200

    Initial Commit

---
+++ PokedexAPI/Models/Custom/CustomCell.swift
@@ -0,0 +1,60 @@
+import UIKit
+
+class CustomCell: UITableViewCell {
+    
+    let additionalImageView = UIImageView() // Nuevo imageView añadido
+    let pokemonImageView = UIImageView()
+    let pokedexNumberLabel = UILabel()
+    let pokemonNameLabel = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupComponents()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupComponents() {
+        additionalImageView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(additionalImageView)
+        
+        pokemonImageView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(pokemonImageView)
+        
+        pokedexNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(pokedexNumberLabel)
+        
+        pokemonNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(pokemonNameLabel)
+        
+        // Opcional: Configurar estilos iniciales
+        pokedexNumberLabel.textAlignment = .left
+        pokemonNameLabel.textAlignment = .left
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            additionalImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            additionalImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            additionalImageView.widthAnchor.constraint(equalToConstant: 30),
+            additionalImageView.heightAnchor.constraint(equalToConstant: 30),
+            
+            pokemonImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            pokemonImageView.leadingAnchor.constraint(equalTo: additionalImageView.trailingAnchor, constant: 25),
+            pokemonImageView.widthAnchor.constraint(equalToConstant: 50),
+            pokemonImageView.heightAnchor.constraint(equalToConstant: 50), //
+            
+            pokedexNumberLabel.leadingAnchor.constraint(equalTo: pokemonImageView.trailingAnchor, constant: 20),
+            pokedexNumberLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            pokedexNumberLabel.widthAnchor.constraint(equalToConstant: 100), // Ancho label
+            
+            pokemonNameLabel.leadingAnchor.constraint(equalTo: pokedexNumberLabel.trailingAnchor, constant: 10),
+            pokemonNameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            pokemonNameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10)
+        ])
+    }
+}
+
---
+++ PokedexAPI/Models/PokemonList/PokemonList.swift
@@ -0,0 +1,96 @@
+import UIKit
+
+class PokemonList: UIViewController {
+    
+    let containerView = UIView() // Vista contenedora
+    let tablePokedex = UITableView() // Tabla
+    
+    var pokemonNames: [String] = []
+    var pokemonSprites: [UIImage?] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+        setupComponents()
+        setupConstraints()
+        
+        fetchPokemonData()
+    }
+    
+    func setupComponents() {
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.layer.borderColor = UIColor.black.cgColor
+        containerView.layer.borderWidth = 2.0
+        containerView.layer.cornerRadius = 10  // Añadir esquinas redondeadas
+        containerView.clipsToBounds = true  // Asegurar que los subviews no se salgan del contenedor
+        view.addSubview(containerView)
+        
+        tablePokedex.translatesAutoresizingMaskIntoConstraints = false
+        tablePokedex.dataSource = self
+        tablePokedex.register(CustomCell.self, forCellReuseIdentifier: "CustomCell")
+        containerView.addSubview(tablePokedex)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            tablePokedex.topAnchor.constraint(equalTo: containerView.topAnchor),
+            tablePokedex.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            tablePokedex.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            tablePokedex.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+        ])
+    }
+    
+    func fetchPokemonData() {
+        PokemonAPI.shared.fetchPokemonNames { pokemonNames in
+            guard let pokemonNames = pokemonNames else { return }
+            
+            self.pokemonNames = pokemonNames
+            self.pokemonSprites = Array(repeating: nil, count: pokemonNames.count)
+            
+            DispatchQueue.main.async {
+                self.tablePokedex.reloadData()
+            }
+            
+            for (index, pokemonName) in pokemonNames.enumerated() {
+                let pokemonIndex = index + 1
+                let pokemonResult = PokemonResult(name: pokemonName, url: "\(PokemonAPI.shared.baseURL)pokemon/\(pokemonIndex)/")
+                
+                PokemonAPI.shared.fetchPokemonSprite(for: pokemonResult) { sprite in
+                    self.pokemonSprites[index] = sprite
+                    
+                    DispatchQueue.main.async {
+                        self.tablePokedex.reloadRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension PokemonList: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pokemonNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        
+        cell.additionalImageView.image = UIImage(named: "PixelPokeball")
+        cell.pokedexNumberLabel.text = "No. 00\(indexPath.row + 1)"
+        cell.pokemonNameLabel.text = pokemonNames[indexPath.row]
+        
+        if let sprite = pokemonSprites[indexPath.row] {
+            cell.pokemonImageView.image = sprite
+        } else {
+            cell.pokemonImageView.image = UIImage(named: "placeholder_image") // Opcional: imagen de relleno mientras se carga
+        }
+        
+        return cell
+    }
+}
+
